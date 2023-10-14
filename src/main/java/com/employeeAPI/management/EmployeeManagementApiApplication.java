3abd4ea1fb4feebdab429d76ebf7bf0eb5165c39
olvericc <olv.ericlopes@gmail.com>
2023-10-13 21:44:14 -0300

    feat implements initial configs of my project

---
+++ src/main/java/com/employeeAPI/management/EmployeeManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.employeeAPI.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApiApplication.class, args);
+	}
+
+}
