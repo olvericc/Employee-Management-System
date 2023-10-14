4de45b5205e9696885c35ed6711eeaead6bec275
Eric Lopes Oliveira <olv.ericlopes@gmail.com>
2023-10-14 00:53:02 -0300

    chore: deleting folders -> source code

--- src/main/java/com/employeeAPI/management/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.employeeAPI.management.repository;
-
-import com.employeeAPI.management.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer>
-{}
