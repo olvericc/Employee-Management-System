e9307f4b2a524374bc72af67cc8ebb8541509d04
olvericc <olv.ericlopes@gmail.com>
2023-10-14 02:59:30 -0300

    feat: adding new routes and navigation for the system

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,22 @@ import HeaderComponent from "./component/HeaderComponent";
 import FooterComponent from "./component/FooterComponent";
 import ListEmployeeComponent from "./component/ListEmployeeComponent";
 import AddEmployeeComponent from "./component/AddEmployeeComponent";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-        <HeaderComponent/>
-        <AddEmployeeComponent/>
-        <ListEmployeeComponent/>
-        <FooterComponent/>
-    </div>
+   return (
+     <BrowserRouter>
+       <HeaderComponent />
+         <div className="container">
+           <Routes>
+             <Route path="" element={<ListEmployeeComponent />} />
+             <Route path="/employee" element={<AddEmployeeComponent />} />
+             <Route path="/add-employee" element={<AddEmployeeComponent />} />
+             <Route path="/add-employee/:id" element={<AddEmployeeComponent />} />
+           </Routes>
+         </div>
+         <FooterComponent />
+     </BrowserRouter>
   );
 }
 
--- frontend/src/component/ListEmployeeComponent.js
+++ frontend/src/component/ListEmployeeComponent.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const ListEmployeeComponent = () =>
 {
     return (
         <div className='container'>
-            <a className='btn btn-dark mb-5 mt-5 align-items-lg-center' href="">Add New Employee</a>
+            <Link to={"/add-employee"} className='btn btn-dark mb-5 mt-5 align-items-lg-center' href="">Add New Employee</Link>
             <h2 className='text-center mb-4 mt-4 '>List of Employees</h2>
             <table className='table table-bordered table striped'>
                 <thead>
